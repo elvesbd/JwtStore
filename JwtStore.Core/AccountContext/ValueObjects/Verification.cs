0326f0ceb2a21393674a227ba98404401df96e43
elvesbd <elvesbd@yahoo.com>
2023-09-04 17:59:12 -0300

    feat: Developing a value object to handle email verification.

---
+++ JwtStore.Core/AccountContext/ValueObjects/Verification.cs
@@ -0,0 +1,26 @@
+using JwtStore.Core.SharedContext.ValueObjects;
+
+namespace JwtStore.Core.AccountContext.ValueObjects;
+
+public class Verification : ValueObject
+{
+    public string Code { get; } = Guid.NewGuid().ToString("N")[0..6].ToUpper();
+    public DateTime? ExpiresAt { get; private set; } = DateTime.UtcNow.AddMinutes(5);
+    public DateTime? VerifiedAt { get; private set; } = null;
+    public bool IsActive => VerifiedAt != null && ExpiresAt == null;
+
+    public void Verify(string code)
+    {
+        if (IsActive)
+            throw new Exception("Este código já foi ativado!");
+
+        if (ExpiresAt < DateTime.UtcNow)
+            throw new Exception("Código expirou!");
+
+        if (!string.Equals(code.Trim(), Code.Trim(), StringComparison.CurrentCultureIgnoreCase))
+            throw new Exception("Código inválido!");
+
+        ExpiresAt = null;
+        VerifiedAt = DateTime.UtcNow;
+    }
+}
