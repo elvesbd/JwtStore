3603414158869e2f5ff3009a2575683a7a835157
elvesbd <elvesbd@yahoo.com>
2023-09-06 11:27:41 -0300

    feat: Developing business logic in services.

--- JwtStore.Core/Configuration.cs
+++ JwtStore.Core/Configuration.cs
@@ -4,10 +4,23 @@ public static class Configuration
 {
     public static SecretsConfiguration Secrets { get; set; } = new();
     public static DatabaseConfiguration Database { get; set; } = new();
+    public static EmailConfiguration Email { get; set; } = new();
+    public static SendGridConfiguration SendGrid { get; set; } = new();
 
     public class DatabaseConfiguration
     {
         public string ConnectionString { get; set; } = string.Empty;
+    }
+
+    public class EmailConfiguration
+    {
+        public string DefaultFromEmail { get; set; } = "test@elvesbd.io";
+        public string DefaultFromName { get; set; } = "elves.bd";
+    }
+
+    public class SendGridConfiguration
+    {
+        public string ApiKey { get; set; } = string.Empty;
     }
 
     public class SecretsConfiguration
