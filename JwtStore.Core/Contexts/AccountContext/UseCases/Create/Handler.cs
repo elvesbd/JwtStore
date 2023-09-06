5d46b83a3a44633b3a9b5a18c4c7617a1ef392cb
elvesbd <elvesbd@yahoo.com>
2023-09-06 10:48:09 -0300

    feat: Creating entities based on your value objects.

--- JwtStore.Core/Contexts/AccountContext/UseCases/Create/Handler.cs
+++ JwtStore.Core/Contexts/AccountContext/UseCases/Create/Handler.cs
@@ -1,5 +1,7 @@
+using JwtStore.Core.Contexts.AccountContext.Entities;
 using JwtStore.Core.Contexts.AccountContext.UseCases.Create.Contracts;
 using JwtStore.Core.Contexts.AccountContext.UseCases.Create.Exceptions;
+using JwtStore.Core.Contexts.AccountContext.ValueObjects;
 
 namespace JwtStore.Core.Contexts.AccountContext.UseCases.Create;
 
@@ -17,6 +19,7 @@ public class Handler
     public async Task<Response> Handle(Request request, CancellationToken cancellationToken)
     {
         #region 01 - Validate request
+
         try
         {
             var res = Specification.Ensure(request);
@@ -27,9 +30,26 @@ public class Handler
         {
             return new Response("Unable to validate your request", 500);
         }
+
         #endregion
 
         #region 02 - Generate objects
+
+        Email email;
+        Password password;
+        User user;
+
+        try
+        {
+            email = new Email(request.Email);
+            password = new Password(request.Password);
+            user = new User(request.Name, email, password);
+        }
+        catch (Exception ex)
+        {
+            return new Response(ex.Message, 400);
+        }
+
         #endregion
 
         #region 03 - Verify if user existis
