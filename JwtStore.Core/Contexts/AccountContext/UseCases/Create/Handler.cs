fa33147d67bcc0d6379473bd565856df1275093d
elvesbd <elvesbd@yahoo.com>
2023-09-06 10:38:55 -0300

    feat: Validate requests

--- JwtStore.Core/Contexts/AccountContext/UseCases/Create/Handler.cs
+++ JwtStore.Core/Contexts/AccountContext/UseCases/Create/Handler.cs
@@ -1,4 +1,5 @@
 using JwtStore.Core.Contexts.AccountContext.UseCases.Create.Contracts;
+using JwtStore.Core.Contexts.AccountContext.UseCases.Create.Exceptions;
 
 namespace JwtStore.Core.Contexts.AccountContext.UseCases.Create;
 
@@ -15,6 +16,29 @@ public class Handler
 
     public async Task<Response> Handle(Request request, CancellationToken cancellationToken)
     {
+        #region 01 - Validate request
+        try
+        {
+            var res = Specification.Ensure(request);
+            if (!res.IsValid)
+                return new Response("Invalid request", 400, res.Notifications);
+        }
+        catch
+        {
+            return new Response("Unable to validate your request", 500);
+        }
+        #endregion
 
+        #region 02 - Generate objects
+        #endregion
+
+        #region 03 - Verify if user existis
+        #endregion
+
+        #region 04 - Persist data
+        #endregion
+
+        #region 05 - Send welcome e-mail
+        #endregion
     }
 }
