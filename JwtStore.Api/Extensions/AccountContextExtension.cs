1483c5717a44ceab13d112d1f5a7a5889ad526c9
elvesbd <elvesbd@yahoo.com>
2023-09-06 11:44:45 -0300

    feat: Registering dependency injection dependencies.

--- JwtStore.Api/Extensions/AccountContextExtension.cs
+++ JwtStore.Api/Extensions/AccountContextExtension.cs
@@ -3,7 +3,23 @@ namespace JwtStore.Api.Extensions;
 public static class AccountExtension
 {
     public static void AddAccountContext(this WebApplicationBuilder builder)
-    { }
+    {
+        #region Create
+
+        builder.Services.AddTransient
+        <
+            JwtStore.Core.Contexts.AccountContext.UseCases.Create.Contracts.IRepository,
+            JwtStore.Infra.Contexts.AccountContext.UseCases.Create.Repository
+        >();
+
+        builder.Services.AddTransient
+        <
+            JwtStore.Core.Contexts.AccountContext.UseCases.Create.Contracts.IRepository,
+            JwtStore.Infra.Contexts.AccountContext.UseCases.Create.Repository
+        >();
+
+        #endregion
+    }
 
     public static void MapAccountEndpoints(this WebApplication builder)
     { }
