55eaa70e7017c6c412546a9b109f5754da16a4d7
elvesbd <elvesbd@yahoo.com>
2023-09-06 12:02:07 -0300

    feat: Creating the POST method for authentication.

--- JwtStore.Api/Extensions/AccountContextExtension.cs
+++ JwtStore.Api/Extensions/AccountContextExtension.cs
@@ -1,3 +1,6 @@
+using JwtStore.Core.Contexts.AccountContext.UseCases.Create;
+using MediatR;
+
 namespace JwtStore.Api.Extensions;
 
 public static class AccountExtension
@@ -21,11 +24,21 @@ public static class AccountExtension
         #endregion
     }
 
-    public static void MapAccountEndpoints(this WebApplication builder)
+    public static void MapAccountEndpoints(this WebApplication app)
     {
         #region Create
 
-
+        app.MapPost("api/v1/users", async
+        (
+            Request request,
+            IRequestHandler<Request, Response> handler
+        ) =>
+        {
+            var result = await handler.Handle(request, new CancellationToken());
+            return result.IsSuccess
+                ? Results.Created("", result)
+                : Results.Json(result, statusCode: result.Status);
+        });
 
         #endregion
     }
